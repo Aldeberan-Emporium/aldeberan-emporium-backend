8c833a90d0587a9520c2464ea2b00239fa17206b
SC ONG <proscawards99@gmail.com>
2021-10-01 14:28:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
             $result = mysqli_query($conn, $query);
             if (mysqli_num_rows($result) > 0) {
                 while($row = mysqli_fetch_assoc($result)){
-                    $data[] = $row;
+                    $data[] = utf8_decode(urldecode($row));
                 }
                 echo json_encode($data);
             }
