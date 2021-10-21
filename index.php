7a931e6dc91ae24ccafca3cb31d903aabaec07cf
SC ONG <proscawards99@gmail.com>
2021-10-21 16:50:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -332,7 +332,7 @@
                         while($row1 = mysqli_fetch_assoc($result1)){
                             $row = json_encode($row);
                             $row = json_decode($row, TRUE);
-                            $row[] = ['total_items' => $row1['total_items']];
+                            $row[] = $row1;
                             $data[] = $row;
                         }
                     }
