eeeba9cab5eb50505a0f704c2d835f6705bf1743
KelvinDesktop <39939811+wkyg@users.noreply.github.com>
2021-10-24 14:37:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -422,7 +422,7 @@
             $result = mysqli_query($conn, $query);
             if (mysqli_num_rows($result) > 0){
                 while($row = mysqli_fetch_assoc($result)){
-                    $data[] = $row;w
+                    $data[] = $row;
                 }
                 echo json_encode($data);
             }
